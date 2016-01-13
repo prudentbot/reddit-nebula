b109aff8d5cb97e977075646d75c43a8a2b30656
Tyler Smith <tylsmith@gmail.com>
2016-01-12 17:30:25 -0800

    fixed deployment things

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var rest = require('../rest');
 var router = express.Router();
 var path = require("path");
 /* GET home page. */
@@ -8,16 +7,11 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.resolve("./public/intro.html"));
 });
 
-router.get('/graph', function(req, res, next) {
-  console.log("graph")
-  console.log(req.body.threadurl);
-  rest.getJSON(req.body.url, function(err, result){
-    if(err || !result){
-      res.send(err);
-      return
-    }
-    console.log(result);
-  });
-});
+// router.get('/graph', function(req, res, next) {
+//   console.log("graph")
+//   console.log(req.body.threadurl);
+//     console.log(result);
+//   });
+// });
 
 module.exports = router;
